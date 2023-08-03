0d3ab87cbf5dbf77a2a7bd678051a522ed544017
golden-state0fmind <areyes2009@yahoo.com>
2023-08-03 16:45:16 -0700

    refactored how options are rendered and added callback function to select add-ons

--- src/app/components/PickAddOns.tsx
+++ src/app/components/PickAddOns.tsx
@@ -1,39 +1,51 @@
+import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { setPlan } from "../store/planSlice";
 import { IBillingPlan } from "./SelectPlan";
-import { Footer, PlanOption } from "./utilities"
+import { AddOn, Footer } from "./utilities"
 
 const PickAddOns = () => {
     const dispatch = useAppDispatch()
     const currentPlan = useAppSelector(state => state.trackPlan.value)
     const currentStep = useAppSelector(state => state.stepTracker.value)
 
-    const handlePlanSelect = (plan: IBillingPlan[keyof IBillingPlan]) => {
-        console.log(plan, 'current plan from pick add ons')
-        //TODO-->
-        // 1. Update state with the new services
-        // 2. Build the selections
-        // 3. Build helper functions 
-        // 4. 
+    const initialAddOns = [
+        { name: 'Online Service', price: 1, description: 'Access to multiplayer games.', selected: false },
+        { name: 'Larger storage', price: 2, description: 'Extra 1TB of cloud storage.', selected: false },
+        { name: 'Customizable profile', price: 2, description: 'Custom theme on your profile.', selected: false }
+        // Add more add-ons here
+    ];
+
+    const [addOns, setAddOns] = useState(initialAddOns);
+
+    const handlePlanSelect = (addOnName: string, index: number) => { //plan: IBillingPlan[keyof IBillingPlan]
+        
+        initialAddOns[index].selected = !initialAddOns[index].selected
+        
+        const updatedAddOns = addOns.map(addOn => {
+            if (addOn.name === addOnName) {
+                return { ...addOn, selected: !addOn.selected };
+            }
+            return addOn;
+        });
+        setAddOns(updatedAddOns);
+        // You can dispatch actions or perform other tasks related to the selected add-on here
+        
+        return
     };
-    console.log(currentPlan, currentStep, '<<step and currentPlan')
 
     return (
         <>
             <div className='p-5 flex flex-col items-center'>
                 <div className="w-full max-w-sm">
                     <h1 className="text-2xl font-semibold text-blue-900 pt-1">Pick add-ons</h1>
-                    <p className="text-gray-500 py-2 text-xl">Add-ons help enhancce your gaming experience.</p>
+                    <p className="text-gray-500 py-2 text-xl">Add-ons help enhance your gaming experience.</p>
                 </div>
-                <div className='w-full' onClick={() => handlePlanSelect(currentPlan)}>
-                    <PlanOption name={'Online Service'} price={1} monthlyBilling={currentPlan.monthlyBill} imageSrc={''} />
-                </div>
-                <div className='w-full' onClick={() => handlePlanSelect(currentPlan)}>
-                    <PlanOption name={'Larger storage'} price={2} monthlyBilling={currentPlan.monthlyBill} imageSrc={''} />
-                </div>
-                <div className='w-full' onClick={() => handlePlanSelect(currentPlan)}>
-                    <PlanOption name={'Customizable profile'} price={2} monthlyBilling={currentPlan.monthlyBill} imageSrc={''} />
-                </div>
+                {addOns.map((addOn, index) => (
+                    <div className={`w-full ${index === 1 ? 'py-4' : ''}`} key={addOn.name} onClick={() => handlePlanSelect(addOn.name, index)}>
+                        <AddOn name={addOn.name} price={addOn.price} monthlyBilling={currentPlan.monthlyBill} description={addOn.description} checked={addOn.selected} />
+                    </div>
+                ))}
                 <Footer currentStep={currentStep} />
             </div>
         </>
